b456d8fb4bf9b02e719c2d2cf5ffa46eda3304b0
Usman Ali <muausman111@gmail.com>
2019-03-08 22:17:13 +0500

    Hospital Management System

---
+++ Hospital Management System/Hospital Management System/Models/UserRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital_Management_System.Models
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
+}
